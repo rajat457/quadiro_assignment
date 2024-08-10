49da4ff86b671d087be8e6086933191c0621d8fd
rajat457 <91472404+rajat457@users.noreply.github.com>
2024-08-10 13:27:02 +0530

    Update dp.php

--- dp.php
+++ dp.php
@@ -1,12 +1,13 @@
 <?php
-$servername = "dpg-cqrho7ggph6c739ve1n0-a";
-$username = "root"; // Default XAMPP username
-$password = "NsRr511aPIRDaJt4HcLmgEy5jWJ6g0IA"; // Default XAMPP password
-$dbname = "quadiro_db";
+$dsn = 'postgresql://root:NsRr511aPIRDaJt4HcLmgEy5jWJ6g0IA@dpg-cqrho7ggph6c739ve1n0-a/quadiro_db';
+$username = 'if0_37077200';
+$password = 'your_password';
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
 ?>
