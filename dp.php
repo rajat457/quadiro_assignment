03590fa87ca5f31a54c730554d59e52451295aa3
rajat457 <91472404+rajat457@users.noreply.github.com>
2024-08-10 13:56:31 +0530

    Update dp.php

--- dp.php
+++ dp.php
@@ -1,7 +1,11 @@
 <?php
-$dsn = 'postgresql://root:NsRr511aPIRDaJt4HcLmgEy5jWJ6g0IA@dpg-cqrho7ggph6c739ve1n0-a.oregon-postgres.render.com/quadiro_db';
+$host = 'dpg-cqrho7ggph6c739ve1n0-a.oregon-postgres.render.com';
+$port = '5432'; // Default PostgreSQL port
+$dbname = 'quadiro_db';
 $username = 'root';
 $password = 'NsRr511aPIRDaJt4HcLmgEy5jWJ6g0IA';
+
+$dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
 
 try {
     $pdo = new PDO($dsn, $username, $password);
