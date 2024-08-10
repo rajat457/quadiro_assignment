9fde5a5883e860e984f9a56e1ab41c02635e2583
rajat457 <91472404+rajat457@users.noreply.github.com>
2024-08-10 14:52:52 +0530

    Update dp.php

--- dp.php
+++ dp.php
@@ -4,11 +4,11 @@ $username = "if0_37077798";
 $password = "IbUh9VlbleeE";
 $dbname = "if0_37077798_quadiro_db";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    echo "Connection failed: " . $conn->connect_error;
+} else {
+    echo "Connected successfully";
 }
 ?>
