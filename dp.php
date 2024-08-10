43933aa6877f47b3e17dd879ffe6db21c80d0eb6
rajat457 <91472404+rajat457@users.noreply.github.com>
2024-08-10 13:24:05 +0530

    Update dp.php

--- dp.php
+++ dp.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
+$servername = "dpg-cqrho7ggph6c739ve1n0-a";
 $username = "root"; // Default XAMPP username
-$password = ""; // Default XAMPP password
+$password = "NsRr511aPIRDaJt4HcLmgEy5jWJ6g0IA"; // Default XAMPP password
 $dbname = "quadiro_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
