45bcd7826b10be46d994142244c6c2d270370129
rajat457 <91472404+rajat457@users.noreply.github.com>
2024-08-10 13:46:40 +0530

    Update add_car.php

--- controllers/add_car.php
+++ controllers/add_car.php
@@ -1,12 +1,19 @@
 <?php
 include '../models/car.php';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+// Display errors for debugging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $carName = $_POST['car_name'];
     $manufacturingYear = $_POST['manufacturing_year'];
     $price = $_POST['price'];
 
     createCar($carName, $manufacturingYear, $price);
-    header("Location: ../views/dashboard.php");
+
+    header('Location: ../views/dashboard.php');
+    exit();
 }
 ?>
